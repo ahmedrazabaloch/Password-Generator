553dc96d5e9c1e6d78ffaabfc4ec329ab7adf44b
Ahmed Raza <ahmed.raza1054@gmail.com>
2023-10-09 01:40:39 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// var paas = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+";
+// var generatePass = "";
+// for (i = 0; i < 12; i++) {
+//   var pass = paas[Math.floor(Math.random() * paas.length)];
+//   generatePass += pass;
+// }
+// document.write(generatePass);
+
+// var password = ["ABCD", "abcd", "!@#$"];
+// var generatePass = "";
+// for (i = 0; i < password.length; i++) {
+//   var pass = password[Math.floor(Math.random() * password.length)];
+//   generatePass += pass;
+// }
+// document.write(generatePass);
+
