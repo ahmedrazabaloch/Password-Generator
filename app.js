345792b3a92b5280f5160bd11d0658e0821112b6
ahmedraza <ahmed.raza1054@gmail.com>
2023-11-08 03:53:29 +0500

    function added for checkbox

--- app.js
+++ app.js
@@ -14,3 +14,47 @@
 // }
 // document.write(generatePass);
 
+var UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var SpecialCharacters = "!@#$%^&*(/)_<=]{,+}[|;.>:'?-";
+var Numbers = "0123456789";
+
+var smallAlpha = document.querySelectorAll("#inlineCheckbox1");
+var capitalAlpha = document.querySelectorAll("#inlineCheckbox2");
+var specialChara = document.querySelectorAll("#inlineCheckbox3");
+var number = document.querySelectorAll("#inlineCheckbox4");
+
+function getCheckedValues(checkboxes) {
+  var checkedValue = [];
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checkedValue.push(checkboxes[i].value);
+    }
+  }
+  return checkedValue;
+}
+
+// var smallAlphaCheckboxes = document.querySelectorAll("#inlineCheckbox1");
+// var capitalAlphaCheckboxes = document.querySelectorAll("#inlineCheckbox2");
+// var specialCharaCheckboxes = document.querySelectorAll("#inlineCheckbox3");
+// var numberCheckboxes = document.querySelectorAll("#inlineCheckbox4");
+
+// function getCheckedValues(checkboxes) {
+//   var checkedValues = [];
+//   for (var i = 0; i < checkboxes.length; i++) {
+//     if (checkboxes[i].checked) {
+//       checkedValues.push(checkboxes[i].value);
+//     }
+//   }
+//   return checkedValues;
+// }
+
+var smallAlphaValues = getCheckedValues(smallAlphaCheckboxes);
+var capitalAlphaValues = getCheckedValues(capitalAlphaCheckboxes);
+var specialCharaValues = getCheckedValues(specialCharaCheckboxes);
+var numberValues = getCheckedValues(numberCheckboxes);
+
+console.log("Small Alpha Values: " + smallAlphaValues);
+console.log("Capital Alpha Values: " + capitalAlphaValues);
+console.log("Special Character Values: " + specialCharaValues);
+console.log("Number Values: " + numberValues);
