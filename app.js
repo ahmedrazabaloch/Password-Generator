58b16bf141dd581f4f750b79e3dc6dd4cda67446
ahmedraza <ahmed.raza1054@gmail.com>
2023-11-13 22:49:38 +0500

    function added

--- app.js
+++ app.js
@@ -14,26 +14,6 @@
 // }
 // document.write(generatePass);
 
-var UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var SpecialCharacters = "!@#$%^&*(/)_<=]{,+}[|;.>:'?-";
-var Numbers = "0123456789";
-
-var smallAlpha = document.querySelectorAll("#inlineCheckbox1");
-var capitalAlpha = document.querySelectorAll("#inlineCheckbox2");
-var specialChara = document.querySelectorAll("#inlineCheckbox3");
-var number = document.querySelectorAll("#inlineCheckbox4");
-
-function getCheckedValues(checkboxes) {
-  var checkedValue = [];
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      checkedValue.push(checkboxes[i].value);
-    }
-  }
-  return checkedValue;
-}
-
 // var smallAlphaCheckboxes = document.querySelectorAll("#inlineCheckbox1");
 // var capitalAlphaCheckboxes = document.querySelectorAll("#inlineCheckbox2");
 // var specialCharaCheckboxes = document.querySelectorAll("#inlineCheckbox3");
@@ -49,12 +29,65 @@ function getCheckedValues(checkboxes) {
 //   return checkedValues;
 // }
 
-var smallAlphaValues = getCheckedValues(smallAlphaCheckboxes);
-var capitalAlphaValues = getCheckedValues(capitalAlphaCheckboxes);
-var specialCharaValues = getCheckedValues(specialCharaCheckboxes);
-var numberValues = getCheckedValues(numberCheckboxes);
+var UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var SpecialCharacters = "!@#$%^&*(/)_<=]{,+}[|;.>:'?-";
+// var Numbers = "0123456789";
 
-console.log("Small Alpha Values: " + smallAlphaValues);
-console.log("Capital Alpha Values: " + capitalAlphaValues);
-console.log("Special Character Values: " + specialCharaValues);
-console.log("Number Values: " + numberValues);
+var smallAlpha = document.querySelectorAll("#smallCheckBox");
+var capitalAlpha = document.querySelectorAll("#capitalCheckBox");
+var specialChara = document.querySelectorAll("#specialCheckBox");
+// var number = document.querySelectorAll("#numberCheckBox");
+
+function getCheckedValues(checkboxes) {
+  var checkedValue = [];
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checkedValue.push(checkboxes[i].value);
+    }
+  }
+  return checkedValue;
+}
+
+var smallAlphaValues = getCheckedValues(smallAlpha);
+var capitalAlphaValues = getCheckedValues(capitalAlpha);
+var specialCharaValues = getCheckedValues(specialChara);
+// var numberValues = getCheckedValues(number);
+
+// if (numberValues) {
+//   pass.push(Numbers);
+// }
+
+function generatePass() {
+  var pass = [];
+  var generatePass = "";
+
+  if (smallAlphaValues.check) {
+    pass.push(LowercaseLetters);
+    console.log(smallAlphaValues);
+  }
+  if (capitalAlphaValues.check) {
+    pass.push(UppercaseLetters);
+    console.log(capitalAlphaValues);
+  }
+  if (specialCharaValues) {
+    pass.push(SpecialCharacters);
+    console.log(specialCharaValues);
+  }
+
+  console.log("after push ==>",pass);
+
+  for (i = 0; i < 6; i++) {
+    var getPass = pass[Math.floor(Math.random() * pass.length)];
+    generatePass += getPass;
+  }
+
+  var displayPass = document.getElementById("displayPass");
+  displayPass.value = generatePass;
+  console.log(generatePass);
+}
+
+// console.log("Small Alpha Values: " + smallAlphaValues);
+// console.log("Capital Alpha Values: " + capitalAlphaValues);
+// console.log("Special Character Values: " + specialCharaValues);
+// console.log("Number Values: " + numberValues);
